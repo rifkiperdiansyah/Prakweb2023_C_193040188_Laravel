7667d660da9c65a91e7ba466cda91be73adbac93
Rifki Perdiansyah <rifkiperdiansyah99@gmail.com>
2023-10-31 02:28:06 -0700

    routing
    menyelesaikan video routing

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>About Blog Saya</title>
+</head>
+<body>
+    <h1>Halaman aboout</h1>
+
+    <h1><?= $nama; ?> </h1>
+    <p><?= $email;  ?></p>
+    
+</body>
+</html>

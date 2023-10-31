b5c994d0f97ec9e20d8ee8194ad4d75a8030efc1
Rifki Perdiansyah <rifkiperdiansyah99@gmail.com>
2023-10-31 04:10:09 -0700

    Video 4 & 5
    selesai video 4 tamlate angine & video 5 model, collection dan controller

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,6 +8,22 @@
     <title>Blog Saya || POSTS</title>
 </head>
 <body>
-    <h1>Halaman posts</h1>
+@extends('layouts.main')
+
+@section('container')
+
+  @foreach ($posts as $post)
+
+    <article class="mb-5">
+      <h2>
+        <a href="/posts/{{ $post['sl'] }}">{{ $post["title"] }}</a>
+      </h2>
+      <h5>By: {{ $post["author"] }}</h5>
+      <p>{{ $post["body"] }}</p>
+    </article>
+
+  @endforeach
+
+@endsection
 </body>
 </html>
